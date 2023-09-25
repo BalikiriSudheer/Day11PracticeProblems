15778236c0298dab1aa88eaf8344f8560516b974
BalikiriSudheer <sudheerbalikiri2000@gmail.com>
2023-09-25 23:09:11 +0530

    [Sudheer]Add:String

---
+++ Reverse/Reverse/Reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    internal class Reverse
+    {
+        public void String()
+        {
+            Console.WriteLine("Please enter the Sentence");
+            string username= Console.ReadLine();
+            string reversedString = " ";
+            for(int i =username.Length-1; i >= 0; i--)
+            {
+                reversedString += username[i];
+            }
+            Console.WriteLine("Reversed String is :"+reversedString);
+        }
+    }
+}
