0253aa3a0594b0bf679a7026b81ff6f05719a63d
BalikiriSudheer <sudheerbalikiri2000@gmail.com>
2023-09-25 22:34:12 +0530

    [Sudheer]Add:sum

---
+++ SumDigits/SumDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int n=int.Parse(Console.ReadLine());
+            Sum s=new Sum();
+            s.SumDigits(n);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SumDigits/SumDigits/Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigits
+{
+    internal class Sum
+    {
+        public void SumDigits(int n)
+        {
+            int sum = 0;
+            while (n != 0)
+            {
+                int d = n % 10;
+                sum = sum + d;
+                n = n / 10;
+            }
+            Console.WriteLine(sum);
+
+        }
+
+    }
+        }
+ 
