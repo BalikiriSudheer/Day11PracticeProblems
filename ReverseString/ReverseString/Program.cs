0f2fec07d287fa767a3435f4f4c92332c9550232
BalikiriSudheer <sudheerbalikiri2000@gmail.com>
2023-09-25 22:15:18 +0530

    [Sudheer]Add:Reverse

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            
+            string result = string.Empty;
+            Console.Write("Enter the String");
+            string st=Console.ReadLine();
+            string[] arr = st.Split();
+            
+            for(int i=0; i<arr.Length; i++) {
+                if (i != arr.Length - 1)
+                {
+                    result = result + ReverseString(arr[i]) + " ";
+
+                }
+                else
+                {
+                    result = result + ReverseString(arr[i]);
+                }
+                Console.WriteLine("Reverse a string:{0}",result);
+                Console.ReadLine();
+            }
+
+        }
+
+        static string ReverseString(string st)
+        {
+            string result = " ";
+            if (string.IsNullOrEmpty(st))
+                return string.Empty; ;
+            for (int i = st.Length - 1; i >= 0; i--)
+            {
+                result = result + st[i];
+
+
+            }
+            return result;
+            ;
+        }
+    }
+    }
+
---
+++ ReverseString/ReverseString/ReverseString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class ReverseString
+    { public String Reverse(String st)
+        {
+            String result = " ";
+            if (String.IsNullOrEmpty(st))
+                return String.Empty; ;
+            for (int i = st.Length - 1; i >= 0; i--)
+            {
+                result = result + st[i];
+
+
+            }
+            return result;
+
+        } }
+}
